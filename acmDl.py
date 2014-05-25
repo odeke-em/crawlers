d633305c7f29dd0bb1def85f54dcedb561ba3fc1
Emmanuel Odeke <emmanuel.odeke@cmgcanada.ca>
2014-05-25 00:58:23 -0600

    Simple clean up

--- acmDl.py
+++ acmDl.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-
 #Author: Emmanuel Odeke <odeke@ualberta.ca>
 #        Download the problem sets for the ACM finals for various years
 
 import urllib.request, re
-url = "http://www.acmicpc.org/worldfinals/problems"
 
-data = urllib.request.urlopen( url )
-readData = ( data.read() ).decode()
-readData = re.sub( "[\n\t]","", readData ) #Take out tabs and newlines
+probsCompile = re.compile('"(http://[^"]*/problems/[^"]*\.pdf)"')
 
-PROBS_REGEX  = '"(http://[^"]*/problems/[^"]*\.pdf)"'
-probsCompile = re.compile( PROBS_REGEX )
+data = urllib.request.urlopen("http://www.acmicpc.org/worldfinals/problems")
+readData = data.read().decode()
+readData = re.sub("[\n\t]", "", readData) # Take out tabs and newlines
 
-matches = probsCompile.findall( readData )
-#print( matches )
+matches = probsCompile.findall(readData)
+
 for match in matches:
-    nameQuery = re.search( ".*/problems/(.*pdf)", match )
+    nameQuery = re.search(".*/problems/(.*pdf)", match)
     if nameQuery:
-       dlData = urllib.request.urlopen( match )
+       dlData = urllib.request.urlopen(match)
        outData = dlData.read()
-       fileName = ( nameQuery.groups( 1 )[0])
-       f = open( fileName, 'wb' )
-       f.write( outData )
+       fileName = (nameQuery.groups(1)[0])
 
-       f.flush()
-       f.close()
-       print( "%s written to memory"%( fileName ))
+       with open(fileName, 'wb') as f:
+         f.write(outData) and print("%s written to memory"%(fileName))
