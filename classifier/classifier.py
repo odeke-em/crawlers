c8e9310287cec439c32c1af6a7d1ef486945c04a
Emmanuel Odeke <emmanuel.odeke@cmgcanada.ca>
2014-07-16 21:08:19 -0600

    Vamped classifier

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 # Author: Emmanuel Odeke <odeke@ualberta.ca>
 
+import os
 import sys
+import time
 import random
 
 WORD_RANK_CACHE = dict() # Useful to memoize expensively computed ranks
+fOpenArgs = {}
+pyVersion = int(sys.hexversion/(1<<24))
+if pyVersion >= 3:
+    fOpenArgs = {'encoding': 'utf-8'}
 
 class DynaItem:
     # Enables application of the '.' method for attributes passed in
-    def __init__(self, attrDict=None):
-        if isinstance(attrDict, dict):
-            for attr, value in attrDict.items():
-                setattr(self, attr, value)
+    def __init__(self, **attrDict):
+        for attr, value in attrDict.items():
+            setattr(self, attr, value)
 
     def __str__(self):
         return self.__dict__.__str__()
@@ -46,7 +51,7 @@ def rankWords(subject, query):
     subjectDict = getWordDict(subject)
 
     statDict = DynaItem(
-        dict(deletions=0, inplace=0, additions=0, moves=0)
+        deletions=0, inplace=0, additions=0, moves=0
     )
 
     for qCh, indexDict in queryDict.items():
@@ -71,70 +76,110 @@ def rankWords(subject, query):
 
     return statDict
 
-def readInFileContent(path):
+def readInFileContent(pathList):
     # Extract the content from a file and create a map of each word
     # and it's unique locations ie line number, word index on the line
-    if not path and os.path.exists(path):
-        return dict(reason='%s is a non existant path'%(path))
-    else:
-        wordsDict = dict()
-        with open(path) as f:
-            lineNumber = 0
-            for l in f.readlines():
-                l = l.strip('\n').strip(' ')
-                lineNumber += 1
-                lineContent = filter(lambda e: e, l.split(' '))
-                lWordIndex = 0 # Local word index
-                for w in lineContent:
-                    wItem = wordsDict.get(w, [])
-                    locationStateInfo = dict(
-                        lineNumber=lineNumber, indexOnLine=lWordIndex
-                    )
-                    wItem.append(locationStateInfo)
-                    if not wItem is None:
-                        wordsDict[w] = wItem
+    if not pathList:
+        return {'reason': 'Expecting a pathList'}
 
-                    lWordIndex += 1
-    
+    paths = set(
+        filter(lambda p: p and os.path.exists(p) and os.path.isfile(p), pathList)
+    )
+    if not paths:
+        return {'reason': 'No valid path could be found'}
+
+    pLen = len(paths)
+    wordsDict = dict()
+    start = time.time()
+    for i, path in enumerate(paths):
+        with open(path, **fOpenArgs) as f:
+            lineno = 0
+            try:
+                for l in f:
+                    l = l.strip('\n').strip(' ')
+                    lineno += 1
+                    lineContent = filter(lambda e: e, l.split(' '))
+                    charno = 0 # Local word index
+                    for w in lineContent:
+                        wItem = wordsDict.get(w, [])
+                        locationStateInfo = dict(
+                            lineno=lineno, charno=charno, source=path
+                        )
+                        wItem.append(locationStateInfo)
+                        if not wItem is None:
+                            wordsDict[w] = wItem
+
+                        charno += 1
+
+            except Exception: # TODO: Handle this error
+                sys.stderr.write(
+                    '\033[91mSkipping processing of: %s\033[00m\n'%(path)
+                )
+
+        sys.stdout.write('Processed: %d/%d files in %2.2f seconds\r'%(
+            i + 1, pLen, time.time()-start)
+        )
+    sys.stdout.write(
+        '\nFile Processing done in %2.2f seconds\n'%(time.time() - start)
+    )
     return wordsDict
 
 def rankStatDict(stDict):
     return (stDict.inplace * 3) + (stDict.moves * 2) +\
            ((stDict.deletions + stDict.additions) * -1)
 
-def createClusters(contentDict, clusterCount=2):
-    samples = random.sample(contentDict.keys(), clusterCount)
+def createClusters(content, pivotCount=2, summary=False, sorting=False, threshold=0.5, retrPivots=[]):
+    pivots = retrPivots
+    insufficient = lambda p: len(p) < pivotCount
+    while insufficient(pivots):
+        pivots += random.sample(content.keys(), pivotCount - len(pivots))
+        pivots = set(pivots)
 
     clusterDict = dict()
 
-    for randPick in samples:
-        maxStDict = DynaItem(
-            dict(inplace=len(randPick), moves=0, additions=0, deletions=0)
-        )
+    for pivot in pivots:
+        maxStDict = DynaItem(inplace=len(pivot), moves=0, additions=0, deletions=0)
         maxRank = rankStatDict(maxStDict)
         pickList = []
-        for key in contentDict:
-            if key is not randPick:
-                stDict = rankWords(randPick, key)
+
+        for key in content:
+            if key is not pivot:
+                stDict = rankWords(pivot, key)
                 rank = rankStatDict(stDict)
                 percentRank = (float)(rank)/maxRank
-                if percentRank >= 0.5:
+                if percentRank >= threshold:
                     pickList.append(DynaItem(
-                        dict(key=key, rank=percentRank)
+                        key=key, rank=percentRank, occurances=content[key]
                     ))
 
-        print(randPick, pickList)
-        clusterDict[randPick] = pickList
+        if sorting:
+            pickList.sort(key=lambda a: a.rank, reverse=True)
+
+        sys.stdout.write('\033[47m%s\033[00m %s'%(
+            pivot, 'Hits:' 
+        ))
+        if summary:
+            sys.stdout.write(' %d\n'%(len(pickList)))
+        else:
+            sys.stdout.write('\n')
+            for p in pickList:
+                sys.stdout.write('\t%s\n'%(p))
+
+        clusterDict[pivot] = pickList
 
     return clusterDict
 
 def main():
     argc = len(sys.argv)
-    srcPath = sys.argv[1] if argc > 1 else __file__
-    # print(WORD_RANK_CACHE)
+    srcPath = sys.argv[1:] if argc > 1 else [__file__]
     wDict = readInFileContent(srcPath)
-    # print(wDict)
-    clusterDict = createClusters(wDict, 8)
+    clusterDict = createClusters(wDict, threshold=0.85,
+        retrPivots=[
+            'Africa', 'Messi', 'Egypt', 'Hunger', 'NSA', 'Gaza',
+            'Privacy', 'Ukraine', 'Snowden', 'Bloomberg', 'Spam'
+            
+        ]
+    )
 
 if __name__ == '__main__':
     main()
